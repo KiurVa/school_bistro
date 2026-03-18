3487376e2a050c365cb3728eb17cbd74fd75c06c
Kauri <kauri.vaab@gmail.com>
2026-03-18 10:08:13 +0200

    menüü vaade paremaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
+
     {{-- jQuery (kui vajad) --}}
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
